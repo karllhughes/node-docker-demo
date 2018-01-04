b297e1e5841f964c4cb6f9a2964d6e4e3692be20
Karl Hughes <khughes.me@gmail.com>
2018-01-04 13:50:59 -0600

    Updating favicon

--- app/index.js
+++ app/index.js
@@ -14,8 +14,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// Middleware setup
+app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
